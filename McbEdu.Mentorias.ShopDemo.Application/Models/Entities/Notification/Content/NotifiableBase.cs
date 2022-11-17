a884b493f07d14d8cd6b4e1d9e10d5e481b4d888
OtavioVB <otaviovb.developer@gmail.com>
2022-11-17 18:20:22 -0300

    fix: `Notification Description Property`

--- McbEdu.Mentorias.ShopDemo.Application/Models/Entities/Notification/Content/NotifiableBase.cs
+++ McbEdu.Mentorias.ShopDemo.Application/Models/Entities/Notification/Content/NotifiableBase.cs
@@ -8,10 +8,12 @@ public abstract class NotifiableBase
     public List<NotificationItemBase> Notifications { get; }
 
     public TypeNotification TypeNotification { get; }
+    public string TypeNotificationName { get; }
 
     protected NotifiableBase(TypeNotification typeNotification) 
     { 
         TypeNotification = typeNotification;
+        TypeNotificationName = typeNotification.ToString();
         Notifications = new List<NotificationItemBase>();
     }
 
--- McbEdu.Mentorias.ShopDemo.Application/Models/Entities/Notification/Items/NotificationItemBase.cs
+++ McbEdu.Mentorias.ShopDemo.Application/Models/Entities/Notification/Items/NotificationItemBase.cs
@@ -7,11 +7,13 @@ public abstract class NotificationItemBase
     public string Title { get; }
     public string Message { get; }
     public TypeNotification TypeNotification { get; }
+    public string TypeNotificationName { get; }
 
     protected NotificationItemBase(string title, string message, TypeNotification typeNotification)
     {
         Title = title;
         Message = message;
         TypeNotification = typeNotification;
+        TypeNotificationName = TypeNotification.ToString();
     }
 }
