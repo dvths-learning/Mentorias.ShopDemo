24e4a29a116bf934ee658de94ba1cb0b5d129a7d
OtavioVB <otaviovb.developer@gmail.com>
2022-11-17 16:39:52 -0300

    refactor: `Infrascructure Repository Herance`

---
+++ McbEdu.Mentorias.ShopDemo.Infrascructure/Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,57 @@
+﻿using McbEdu.Mentorias.ShopDemo.Domain.Contracts.Infrascructure.Repositories;
+using McbEdu.Mentorias.ShopDemo.Domain.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace McbEdu.Mentorias.ShopDemo.Infrascructure.Data.Repositories;
+
+public class CustomerRepository : IBaseRepository<Customer>
+{
+    protected readonly DataContext _dataContext;
+
+    public CustomerRepository(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    public async Task AddAsync(Customer entity)
+    {
+        await _dataContext.Customers.AddAsync(entity);
+        await _dataContext.SaveChangesAsync();
+    }
+
+    public async Task AddRangeAsync(List<Customer> entity)
+    {
+        await _dataContext.Customers.AddRangeAsync(entity);
+        await _dataContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Customer entity)
+    {
+        if (await _dataContext.Customers.CountAsync() < 1) return;
+
+        _dataContext.Customers.Remove(entity);
+        await _dataContext.SaveChangesAsync();
+    }
+
+    public async Task<List<Customer>> GetAllAsync()
+    {
+        if (await _dataContext.Customers.CountAsync() < 1) return new List<Customer>();
+
+        return await _dataContext.Customers.ToListAsync();
+    }
+
+    public async Task<Customer?> GetAsync(Guid identifier)
+    {
+        if (await _dataContext.Customers.CountAsync() < 1) return null;
+
+        return await _dataContext.Customers.Where(p => p.Identifier == identifier).FirstAsync();
+    }
+
+    public async Task UpdateAsync(Customer entity)
+    {
+        if (await _dataContext.Customers.CountAsync() < 1) return;
+
+        _dataContext.Customers.Update(entity);
+        await _dataContext.SaveChangesAsync();
+    }
+}
--- McbEdu.Mentorias.ShopDemo.Infrascructure/Data/Repositories/Extensions/ExtendsCustomerRepository.cs
+++ McbEdu.Mentorias.ShopDemo.Infrascructure/Data/Repositories/Extensions/ExtendsCustomerRepository.cs
@@ -5,57 +5,8 @@ using Microsoft.VisualBasic;
 
 namespace McbEdu.Mentorias.ShopDemo.Infrascructure.Data.Repositories.Extensions;
 
-public class ExtendsCustomerRepository : IExtendsRepository<Customer>
+public class ExtendsCustomerRepository : CustomerRepository, IExtendsRepository<Customer>
 {
-    private readonly DataContext _dataContext;
-
-    public ExtendsCustomerRepository(DataContext dataContext)
-    {
-        _dataContext = dataContext;
-    }
-
-    public async Task AddAsync(Customer entity)
-    {
-        await _dataContext.Customers.AddAsync(entity);
-        await _dataContext.SaveChangesAsync();
-    }
-
-    public async Task AddRangeAsync(List<Customer> entity)
-    {
-        await _dataContext.Customers.AddRangeAsync(entity);
-        await _dataContext.SaveChangesAsync();
-    }
-
-    public async Task DeleteAsync(Customer entity)
-    {
-        if (await _dataContext.Customers.CountAsync() < 1) return;
-
-        _dataContext.Customers.Remove(entity);
-        await _dataContext.SaveChangesAsync();
-    }
-
-    public async Task<List<Customer>> GetAllAsync()
-    {
-        if (await _dataContext.Customers.CountAsync() < 1) return new List<Customer>();
-
-        return await _dataContext.Customers.ToListAsync();
-    }
-
-    public async Task<Customer?> GetAsync(Guid identifier)
-    {
-        if (await _dataContext.Customers.CountAsync() < 1) return null;
-
-        return await _dataContext.Customers.Where(p => p.Identifier == identifier).FirstAsync();
-    }
-
-    public async Task UpdateAsync(Customer entity)
-    {
-        if (await _dataContext.Customers.CountAsync() < 1) return;
-
-        _dataContext.Customers.Update(entity);
-        await _dataContext.SaveChangesAsync();
-    }
-
     public async Task<bool> VerifyEntityExistsAsync(string information)
     {
         if (await _dataContext.Customers.CountAsync() < 1) return false;
