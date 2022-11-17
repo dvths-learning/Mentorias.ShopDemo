af11c7deb8f772c2011e220403cf4dcdfed4b0dc
OtavioVB <otaviovb.developer@gmail.com>
2022-11-17 14:51:36 -0300

    feat: `Infrascructure Repository Abstraction Extension`

---
+++ McbEdu.Mentorias.ShopDemo.Application/Contracts/Infrascructure/Repositories/Extensions/IExtendsRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace McbEdu.Mentorias.ShopDemo.Domain.Contracts.Infrascructure.Repositories.Extensions;
+
+public interface IExtendsRepository<T> : IBaseRepository<T> where T : class
+{
+    public Task<bool> VerifyEntityExistsAsync(string information);
+    public Task<bool> VerifyEntityExistsAsync(Guid identifier);
+}
