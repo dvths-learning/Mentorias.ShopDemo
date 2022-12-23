803c916cdfdf9bbc6d01c125381190410bab0b7f
Tiago Henrique <tiagohs.dev@gmail.com>
2022-12-22 21:00:41 -0300

    chore: implementa parcialmente o serviço de importação de um Customer

--- src/McbEdu.Mentorias.ShopDemo.Application/Services/Import/Customers/ImportCustomersService.cs
+++ src/McbEdu.Mentorias.ShopDemo.Application/Services/Import/Customers/ImportCustomersService.cs
@@ -1,9 +1,42 @@
+using McbEdu.Mentorias.ShopDemo.Application.Common.Interfaces.Persistence;
+using McbEdu.Mentorias.ShopDemo.Domain.Entities;
+
 namespace McbEdu.Mentorias.ShopDemo.Application.Services.Import.Customers;
 
 public class ImportCustomerService : IImportCustomerService
 {
-    public ImportCustomerResult ImportNewCustomer(string firstName, string lastName, string email, string birthDate)
+    private readonly ICustomerRepository _customerRepository;
+
+    public ImportCustomerService(ICustomerRepository customerRepository)
     {
-        return new ImportCustomerResult(Guid.NewGuid(), firstName, lastName, email, birthDate);
+        _customerRepository = customerRepository;
+    }
+
+    public ImportCustomerResult ImportNewCustomer(
+        string firstName,
+        string lastName,
+        string email,
+        string birthDate)
+    {
+        // 1. checar se o cliente não existe no banco
+        if (_customerRepository.GetCustomerByEmail(email) is not null)
+        {
+            throw new Exception("Customer with given email already exists.");
+        }
+
+        // 2. Criar um novo cliente 
+        var newCustomer = new Customer
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            BirthDate = birthDate
+        };
+
+        // Persistir no bd
+        _customerRepository.AddCustomer(newCustomer);
+
+        // Retornar novo client
+        return new ImportCustomerResult(newCustomer);
     }
 }
