a6d5fbba446ba7d18f378893e59861a12b9dd255
Tiago Henrique <tiagohs.dev@gmail.com>
2022-12-28 11:03:29 -0300

    fix: corrige a configuração do mapster para mapeamento dos dados

--- src/McbEdu.Mentorias.ShopDemo.WebApi/Common/Mapping/ImportOneCustomerMappingConfig.cs
+++ src/McbEdu.Mentorias.ShopDemo.WebApi/Common/Mapping/ImportOneCustomerMappingConfig.cs
@@ -12,8 +12,19 @@ public class ImportOneCustomerMappingConfig : IRegister
     public void Register(TypeAdapterConfig config)
     {
         // No momento apenas mapeio os dados do resultado da importação para as propriedades da entidade. 
-        config.NewConfig<CustomerImportDataRequest, ImportOneCustomerCommand>();
+        config.NewConfig<CustomerImportDataRequest, ImportOneCustomerCommand>()
+            .MapWith(request => new ImportOneCustomerCommand(
+                request.FirstName,
+                request.LastName,
+                request.Email,
+                request.BirthDate));
         config.NewConfig<ImportCustomerResult, CustomerImportDataResponse>()
-            .Map(dest => dest, src => src.Customer);
+            .MapWith(result => new CustomerImportDataResponse(
+                result.Customer.Id,
+                result.Customer.FirstName,
+                result.Customer.LastName,
+                result.Customer.Email,
+                result.Customer.BirthDate
+            ));
     }
 }
